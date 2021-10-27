3273fe67407f2b3ea32ca41610b45651b2de6965
renandecarlo <renan.decarlo@yahoo.com.br>
2021-10-27 07:43:13 -0300

    Update config

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,18 @@ const webpack = require('webpack')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/aiqfome-imagens/' : '/',
+
   transpileDependencies: [
     'vuetify'
   ],
+
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
         'process.browser': 'true'
       }),
     ]
-  }
+  },
+
+  assetsDir: 'assets'
 }
