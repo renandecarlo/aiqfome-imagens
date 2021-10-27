9e69f92007226d55c8fa55b3968c4badfbdc1d1a
renandecarlo <renan.decarlo@yahoo.com.br>
2021-10-27 07:43:43 -0300

    Update images

--- src/assets/resources/maskData.js
+++ src/assets/resources/maskData.js
@@ -1,53 +1,144 @@
 export const data = [
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 165,
+    "maskH": 158,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_Data Set 15.jpg"
+    "file": "fomede_LIBER_Data-Set-15.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 164,
+    "maskH": 157,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_Data Set 6.jpg"
+    "file": "fomede_LIBER_Data-Set-6.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 157,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_açaí.jpg"
+    "file": "fomede_LIBER_açaí.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 157,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_burger.jpg"
+    "file": "fomede_LIBER_burger.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 162,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_comida fit.jpg"
+    "file": "fomede_LIBER_comida-fit.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 160,
+    "maskX": 830,
+    "maskY": 50,
     "circle": true,
-    "file": "fomede_LIBER_cupcake.jpg"
+    "file": "fomede_LIBER_cupcake.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 164,
+    "maskX": 830,
+    "maskY": 49,
     "circle": true,
-    "file": "fomede_LIBER_dadinho.jpg"
+    "file": "fomede_LIBER_dadinho.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 157,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_esfiha.jpg"
+    "file": "fomede_LIBER_esfiha.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 159,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_feijoada.jpg"
+    "file": "fomede_LIBER_feijoada.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 162,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_frango frito.jpg"
+    "file": "fomede_LIBER_frango-frito.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 165,
+    "maskH": 156,
+    "maskX": 830,
+    "maskY": 50,
     "circle": true,
-    "file": "fomede_LIBER_hambúrguer.jpg"
+    "file": "fomede_LIBER_hambúrguer.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 155,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_japa.jpg"
+    "file": "fomede_LIBER_japa.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 165,
+    "maskX": 854,
+    "maskY": 34,
     "circle": true,
     "file": "fomede_LIBER_lanche.png"
   },
@@ -56,46 +147,109 @@ export const data = [
     "file": "fomede_LIBER_macarrão.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 163,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_marmita.jpg"
+    "file": "fomede_LIBER_marmita.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 164,
+    "maskH": 163,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_panqueca.jpg"
+    "file": "fomede_LIBER_panqueca.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 163,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_pastel.jpg"
+    "file": "fomede_LIBER_pastel.png"
   },
   {
     "circle": true,
     "file": "fomede_LIBER_pão.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 163,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_risoto.jpg"
+    "file": "fomede_LIBER_risoto.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 157,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_salada.jpg"
+    "file": "fomede_LIBER_salada.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 160,
+    "maskX": 830,
+    "maskY": 50,
     "circle": true,
-    "file": "fomede_LIBER_salgadinho.jpg"
+    "file": "fomede_LIBER_salgadinho.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 160,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_sanduba.jpg"
+    "file": "fomede_LIBER_sanduba.png"
   },
   {
     "circle": true,
     "file": "fomede_LIBER_sonho.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 163,
+    "maskH": 157,
+    "maskX": 830,
+    "maskY": 51,
     "circle": true,
-    "file": "fomede_LIBER_sorvete.jpg"
+    "file": "fomede_LIBER_sorvete.png"
   },
   {
+    "mask": 1,
+    "width": 1080,
+    "height": 1080,
+    "maskW": 165,
+    "maskH": 164,
+    "maskX": 875,
+    "maskY": 36,
     "circle": true,
     "file": "fomede_LIBER_yakisoba.png"
   },
